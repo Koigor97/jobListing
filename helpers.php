7f30969ee470be0a06fd5b3208901b15a16deb68
Koigor97 <turaysamuel101@yahoo.com>
2024-01-19 10:08:09 +0200

    add helper functions to load view and partials

--- helpers.php
+++ helpers.php
@@ -9,4 +9,24 @@
  function base_path($path = '') {
      return __DIR__ . '/' . $path;
  }
- 
+
+
+/**
+ * This is going to Load the View
+ * 
+ * @param string $name
+ * @return void
+ */
+function loadView($name) {
+    require base_path("views/{$name}.view.php");
+}
+
+/**
+ * This function is going to Load the Partial
+ * 
+ * @param string $name
+ * @return void
+ */
+function loadPartial($name) {
+    require base_path("views/partials/{$name}.php");
+}
