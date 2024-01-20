8e8a9236c07b97d781f2ea4935130f4ac093a8f1
Koigor97 <turaysamuel101@yahoo.com>
2024-01-20 20:43:26 +0200

    refactor index.php

--- public/index.php
+++ public/index.php
@@ -1,17 +1,20 @@
 <?php
 // bringing the helpers.php file
 require '../helpers.php';
-
-
+// gettig the router.php file
 require base_path('Router.php');
+// bringing the Database.php file
+require base_path('Database.php');
 
-// creating a new instance of the Router
+// creating a instance of the Router
 $router = new Router();
 
+// getting the routes
 $routes = require base_path('routes.php');
 
-// getting the current route
+// getting the current URI and HTTP method
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 
+// Route to the requested 
 $router->route($uri, $method);
