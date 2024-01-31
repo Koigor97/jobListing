e30226b9b1254a18c471793790570a7c875989e4
Koigor97 <turaysamuel101@yahoo.com>
2024-01-31 09:56:47 +0200

    remove top-banner partial

--- App/views/error.view.php
+++ App/views/error.view.php
@@ -1,6 +1,6 @@
 <?php loadPartial('head'); ?>
 <?php loadPartial('navbar'); ?>
-<?php loadPartial('top-banner'); ?>
+
 
 <section>
   <div class="container mx-auto p-4 mt-4">
@@ -8,6 +8,7 @@
      <p class="text-center text-2xl mb-4">
         <?= $message ?>
      </p>
+     <a class="block text-center" href="/listings">Go Back To Listings</a>
   </div>
 </section>
 
