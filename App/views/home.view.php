b434c49af7762c8b877ba38da8fced20b1a288cc
Koigor97 <turaysamuel101@yahoo.com>
2024-01-31 09:57:09 +0200

    add changes to home.view file

--- App/views/home.view.php
+++ App/views/home.view.php
@@ -28,10 +28,12 @@
                   >
                 </li>
                 <li class="mb-2">
-                  <strong>Tags:</strong> <?= $listing->tags ?>
+                   <?php if (!empty($listing->tags)): ?>
+                    <strong>Tags:</strong> <?= $listing->tags ?>
+                  <?php endif; ?>
                 </li>
               </ul>
-              <a href="/listing/<?= $listing->id ?>"
+              <a href="/listings/<?= $listing->id ?>"
                 class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200"
               >
                 Details
