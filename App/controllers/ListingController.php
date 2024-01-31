e9219bb0f156553e446bb2709f4704ec4e5add81
Koigor97 <turaysamuel101@yahoo.com>
2024-01-31 09:56:27 +0200

    modify file

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -80,12 +80,12 @@ class ListingController {
 
         $newListingData = array_map('sanitize', $newListingData);
 
-        $requiredFields = ['title', 'description', 'email', 'city', 'state'];
+        $requiredFields = ['title', 'description', 'email', 'city', 'state', 'salary'];
 
         $errors = [];
 
         foreach ($requiredFields as $field) {
-            if (empty($newListingData[$field]) || !Validation::string($newListingData['$field'])) {
+            if (empty($newListingData[$field]) || !Validation::string($newListingData[$field])) {
                 $errors[$field] = ucfirst($field) . ' is required';
             }
         }
@@ -98,10 +98,142 @@ class ListingController {
             $fields = [];
 
             foreach ($newListingData as $field => $value) {
-                $field[] = $field;
+                $fields[] = $field;
+            }
+            $fields = implode(', ', $fields); 
+
+            $values = [];
+
+            foreach ($newListingData as $field => $value) {
+                // convert empty strings to null
+                if (empty($value)) {
+                    $newListingData[$field] = null;
+                }
+                $values[] = ':' . $field;
+            }
+            $values = implode(', ', $values);
+
+            // create query
+            $query = "INSERT INTO listings ({$fields}) VALUES ({$values})";
+
+            // execute query
+            $this->db->query($query, $newListingData);
+            
+            // redirect to home page
+            redirect('/listings');
+        }
+    }
+
+    /**
+     * This will delete a listing
+     * 
+     * @param array $params
+     * @return void
+     */
+    public function destroy($params) {
+        $id = $params['id'];
+
+        $params = [
+            'id' => $id
+        ];
+
+        $listing = $this->db->query("SELECT * FROM listings WHERE id = :id", $params)->fetch();
+
+        if(!$listing) {
+            ErrorController::notFound('Lising not found');
+            return;
+        }
+
+        $this->db->query("DELETE FROM listings WHERE id = :id", $params);
+
+        // Set flash message
+        $_SESSION['success_message'] = 'Listing deleted successfully';
+
+        redirect('/listings');
+    }
+
+    /**
+     * This method show the edit listing page
+     * 
+     * @return void
+     */
+    public function edit($params) {
+        $id = $params['id'] ?? '';
+
+        $params = [
+            'id' => $id
+        ];
+
+        $listing = $this->db->query("SELECT * FROM listings WHERE id = :id", $params)->fetch();
+
+        if(!$listing) {
+            ErrorController::notFound('Lising not found');
+            return;
+        }
+
+        loadView('listings/edit', [
+            'listing' => $listing
+        ]);
+    }
+
+    /**
+     * Update a listing
+     * 
+     * @param array s$param
+     * return void
+     */
+    public function update($params) {
+        $id = $params['id'] ?? '';
+
+        $params = [
+            'id' => $id
+        ];
+
+        $listing = $this->db->query("SELECT * FROM listings WHERE id = :id", $params)->fetch();
+
+        if(!$listing) {
+            ErrorController::notFound('Lising not found');
+            return;
+        }
+
+        $allowedFields = ["title", "description", "salary", "requirements", "benefits", "company", "address", "city", "state", "phone", "email"];
+
+        $updateValues = array_intersect_key($_POST, array_flip($allowedFields));
+       
+        $updateValues = array_map('sanitize', $updateValues);
+
+        $requiredFields = ['title', 'description', 'salary', 'email', 'city', 'state'];
+
+        $errors = [];
+
+        foreach ($requiredFields as $field) {
+            if(empty($updateValues[$field]) || !Validation::string($updateValues[$field])) {
+                $errors[$field] = ucfirst($field) . ' is required';
+            }
+        }
+
+        if(!empty($errors)) {
+            loadView('listing/edit', ['listing' => $listing, 'errors' => $errors]);
+            exit;
+        } else {
+            // submit to data
+            $updateFields = [];
+
+            foreach(array_keys($updateValues) as $field) {
+                $updateFields[] = "{$field} = :{$field}";
             }
 
-            $fields = implode('. ', $fields); 
+            $updateFields = implode(', ', $updateFields);
+
+            $updateQuery = "UPDATE listings SET $updateFields WHERE id = :id";
+            
+            $updateValues['id'] = $id;
+
+            $this->db->query($updateQuery, $updateValues);
+
+            $_SESSION['success_message'] = 'Listing Updated';
+
+            redirect('/listings/' . $id);
         }
     }
 }
