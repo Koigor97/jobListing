c83b834788f4c102f8e5f4c011b4332fd4945fb8
Koigor97 <turaysamuel101@yahoo.com>
2024-01-31 09:54:58 +0200

    add loadPartial and modify url path

--- App/views/listings/show.view.php
+++ App/views/listings/show.view.php
@@ -4,15 +4,17 @@
 
 <section class="container mx-auto p-4 mt-4">
       <div class="rounded-lg shadow-md bg-white p-3">
+        <?= loadPartial('message') ?>
        <div class="flex justify-between items-center">
       <a class="block p-4 text-blue-700" href="/listings">
         <i class="fa fa-arrow-alt-circle-left"></i>
         Back To Listings
       </a>
       <div class="flex space-x-4 ml-4">
-        <a href="/edit" class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded">Edit</a>
+        <a href="/listings/edit/<?= $listing->id ?>" class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded">Edit</a>
         <!-- Delete Form -->
         <form method="POST">
+          <input type="hidden" name="_method" value="DELETE">
           <button type="submit" class="px-4 py-2 bg-red-500 hover:bg-red-600 text-white rounded">Delete</button>
         </form>
         <!-- End Delete Form -->
@@ -57,7 +59,7 @@
         resume.
       </p>
       <a
-        href="mailto:<?= $listing->email ?>
+        href="mailto:<?= $listing->email ?>"
         class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium cursor-pointer text-indigo-700 bg-indigo-100 hover:bg-indigo-200"
       >
         Apply Now
