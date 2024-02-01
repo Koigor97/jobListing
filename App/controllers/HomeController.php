1233c436af69c3640f1dff7b04c9e05e79ad2db7
Koigor97 <turaysamuel101@yahoo.com>
2024-02-01 18:12:44 +0200

    add home controller to handle home view

--- App/controllers/HomeController.php
+++ App/controllers/HomeController.php
@@ -4,19 +4,27 @@ namespace App\Controllers;
 
 use Framework\Database;
 
-class HomeController {
-    protected $db;
+class HomeController
+{
+  protected $db;
 
-    public function __construct()
-    {
-        $config = require base_path('config/db.php');
-        $this->db = new Database($config);
-    }
+  public function __construct()
+  {
+    $config = require basePath('config/_db.php');
+    $this->db = new Database($config);
+  }
 
-    public function index() {
-        $listings = $this->db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
+  /*
+   * Show the latest listings
+   * 
+   * @return void
+   */
+  public function index()
+  {
+    $listings = $this->db->query('SELECT * FROM listings ORDER BY created_at DESC LIMIT 6')->fetchAll();
 
-        loadView('home', [
-            'listings' => $listings]);
-            }
+    loadView('home', [
+      'listings' => $listings
+    ]);
+  }
 }
