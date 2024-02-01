0081aa8b4bec2c55e1c1c0533149df309a822768
Koigor97 <turaysamuel101@yahoo.com>
2024-02-01 18:11:23 +0200

    modify error controller file

--- App/controllers/ErrorController.php
+++ App/controllers/ErrorController.php
@@ -2,31 +2,35 @@
 
 namespace App\Controllers;
 
-class ErrorController {
-    /**
-     * This method show the 404 not found error page
-     * 
-     * @return void
-     */
-    public static function notFound($message = 'Resource not found') {
-        http_response_code(404);
-        loadView('error', [
-            'status' => 404,
-            'message' => $message
-        ]);
-    }
+class ErrorController
+{
+  /*
+   * 404 not found error
+   * 
+   * @return void
+   */
+  public static function notFound($message = 'Resource not found')
+  {
+    http_response_code(404);
 
-    /**
-     * This method show the 403 unauthorized error page
-     * 
-     * @return void
-     */
-    public static function unauthorized($message = 'You are not authorized to view this page') {
-        http_response_code(403);
-        loadView('error', [
-            'status' => 403,
-            'message' => $message
-        ]);
-    }
+    loadView('error', [
+      'status' => '404',
+      'message' => $message
+    ]);
+  }
 
+  /*
+   * 403 unauthorized error
+   * 
+   * @return void
+   */
+  public static function unauthorized($message = 'You are not authorized to view this resource')
+  {
+    http_response_code(403);
+
+    loadView('error', [
+      'status' => '403',
+      'message' => $message
+    ]);
+  }
 }
