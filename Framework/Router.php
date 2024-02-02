372e6a57850a6fa976029a37465e537566ea3d96
Koigor97 <turaysamuel101@yahoo.com>
2024-02-02 23:22:58 +0200

    modify the Router class

--- Framework/Router.php
+++ Framework/Router.php
@@ -3,143 +3,152 @@
 namespace Framework;
 
 use App\Controllers\ErrorController;
+use Framework\Middleware\Authorize;
 
-// creating a class called Router
-class Router {
-    // creating a protected property called routes
-    protected $routes = [];
+class Router
+{
+  protected $routes = [];
 
+  /**
+   * Add a new route
+   *
+   * @param string $method
+   * @param string $uri
+   * @param string $action
+   * @param array $middleware
+   * 
+   * @return void
+   */
+  public function registerRoute($method, $uri, $action, $middleware = [])
+  {
+    list($controller, $controllerMethod) = explode('@', $action);
 
-    /**
-     * This method add a new route
-     * 
-     * @param string $method
-     * @param string $uri
-     * @param string $action
-     * @return void
-     */
-    public function registerRoute($method, $uri, $action) {
-        // getting the controller and the method
-        [$controller, $controllerMethod] = explode('@', $action);
+    $this->routes[] = [
+      'method' => $method,
+      'uri' => $uri,
+      'controller' => $controller,
+      'controllerMethod' => $controllerMethod,
+      'middleware' => $middleware
+    ];
+  }
 
-        // adding the route to the routes array
-        $this->routes[] = [
-            'method' => $method,
-            'uri' => $uri,
-            'controller' => $controller,
-            'controllerMethod' => $controllerMethod
-        ];
+  /**
+   * Add a GET route
+   * 
+   * @param string $uri
+   * @param string $controller
+   * @param array $middleware
+   * @return void
+   */
+  public function get($uri, $controller, $middleware = [])
+  {
+    $this->registerRoute('GET', $uri, $controller, $middleware);
+  }
+
+  /**
+   * Add a POST route
+   * 
+   * @param string $uri
+   * @param string $controller
+   * @param array $middleware
+   * 
+   * @return void
+   */
+  public function post($uri, $controller, $middleware = [])
+  {
+    $this->registerRoute('POST', $uri, $controller, $middleware);
+  }
+
+  /**
+   * Add a PUT route
+   * 
+   * @param string $uri
+   * @param string $controller
+   * @param array $middleware
+   * 
+   * @return void
+   */
+  public function put($uri, $controller, $middleware = [])
+  {
+    $this->registerRoute('PUT', $uri, $controller, $middleware);
+  }
+
+  /**
+   * Add a DELETE route
+   * 
+   * @param string $uri
+   * @param string $controller
+   * @param array $middleware
+   * 
+   * @return void
+   */
+  public function delete($uri, $controller, $middleware = [])
+  {
+    $this->registerRoute('DELETE', $uri, $controller, $middleware);
+  }
+
+  /**
+   * Route the request
+   * 
+   * @param string $uri
+   * @param string $method
+   * @return void
+   */
+  public function route($uri)
+  {
+    $requestMethod = $_SERVER['REQUEST_METHOD'];
+
+    // Check for _method input
+    if ($requestMethod === 'POST' && isset($_POST['_method'])) {
+      // Override the request method with the value of _method
+      $requestMethod = strtoupper($_POST['_method']);
     }
 
-    /**
-     * This method add a GET route
-     * 
-     * @param string $uri
-     * @param string $controller
-     * @return void
-     */
-    function get($uri, $controller) {
-         $this->registerRoute('GET', $uri, $controller);
-    }
+    foreach ($this->routes as $route) {
 
+      // Split the current URI into segments
+      $uriSegments = explode('/', trim($uri, '/'));
 
-    /**
-     * This method add a POST route
-     * 
-     * @param string $uri
-     * @param string $controller
-     * @return void
-     */
-    function post($uri, $controller) {
-        $this->registerRoute('POST', $uri, $controller);
-    }
+      // Split the route URI into segments
+      $routeSegments = explode('/', trim($route['uri'], '/'));
 
+      $match = true;
 
-    /**
-     * This method add a GET route
-     * 
-     * @param string $uri
-     * @param string $controller
-     * @return void
-     */
-    function put($uri, $controller) {
-        $this->registerRoute('PUT', $uri, $controller);
-    }
+      // Check if the number of segments matches
+      if (count($uriSegments) === count($routeSegments) && strtoupper($route['method'] === $requestMethod)) {
+        $params = [];
 
+        $match = true;
 
-    /**
-     * This method add a GET route
-     * 
-     * @param string $uri
-     * @param string $controller
-     * @return void
-     */
-    function delete($uri, $controller) {
-         $this->registerRoute('DELETE', $uri, $controller);
-    }
+        for ($i = 0; $i < count($uriSegments); $i++) {
+          // If the uri's do not match and there is no param
+          if ($routeSegments[$i] !== $uriSegments[$i] && !preg_match('/\{(.+?)\}/', $routeSegments[$i])) {
+            $match = false;
+            break;
+          }
 
-    /**
-     * This function would route the request
-     * 
-     * @param string $uri
-     * @param string $method
-     * @return void
-     */
-    public function route($uri) {
-        $requestMethod = $_SERVER['REQUEST_METHOD'];
-
-        // check for _method in the request
-        if ($requestMethod === 'POST' && isset($_REQUEST['_method'])) {
-            // override the request method
-            $requestMethod = strtoupper($_POST['_method']);
-        }
-         // looping through the routes
-        foreach ($this->routes as $route) {
-            // split the current route uri into an array
-            $uriSegments = explode('/', trim($uri, '/'));
-            // split the route uri into an array
-            $routeSegments = explode('/', trim($route['uri'], '/'));
-
-            $match = true;
-
-            // checking if the route segments and the uri segments are the same
-            if (count($uriSegments) === count($routeSegments) && $requestMethod === strtoupper($route['method'])) {
-                $params = [];
-                $match = true;
-
-                // looping through the route segments
-                for ($i = 0; $i < count($uriSegments); $i++) {
-                    // check if uri does not match the route and the route segment is not a parameter
-                    if ($routeSegments[$i] !== $uriSegments[$i] && !preg_match('/\{(.+?)\}/', $routeSegments[$i])) {
-                        $match = false;
-                        break;
-                    }
-
-                    // check if the route segment is a parameter and add it to the params array
-                    if (preg_match('/\{(.+?)\}/', $routeSegments[$i], $matches)) {
-                        $params[$matches[1]] = $uriSegments[$i];
-                    }
-                }
-
-                if ($match) {
-                         //extracting the controller and the method
-                    $controller = 'App\\Controllers\\' . $route['controller'];
-                    $controllerMethod = $route['controllerMethod'];
-
-                    // instantiating the controller and calling the method
-                    $controllerInstance = new $controller();
-                    $controllerInstance->$controllerMethod($params);
-
-                    return;
-
-                }
-                
-            }
-
+          // Check for the param and add to $params array
+          if (preg_match('/\{(.+?)\}/', $routeSegments[$i], $matches)) {
+            $params[$matches[1]] = $uriSegments[$i];
+          }
         }
 
-        // if the route does not exists
-        ErrorController::notFound();
+        if ($match) {
+          foreach ($route['middleware'] as $middleware) {
+            (new Authorize())->handle($middleware);
+          }
+
+          $controller = 'App\\controllers\\' . $route['controller'];
+          $controllerMethod = $route['controllerMethod'];
+
+          // Instatiate the controller and call the method
+          $controllerInstance = new $controller();
+          $controllerInstance->$controllerMethod($params);
+          return;
+        }
+      }
     }
 
+    ErrorController::notFound();
+  }
 }
